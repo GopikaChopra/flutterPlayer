d8dcc991badbc5e2670422189ad5a39aa41b85a9
Taranjeet <chanataranjeet@gmail.com>
2023-02-27 21:57:10 +0530

    player bug fixes

--- lib/screens/videoplayer/video_player.dart
+++ lib/screens/videoplayer/video_player.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:better_player/better_player.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:new_player/screens/videoplayer/video_player_controllers.dart';
- 
 
 import '../../utils/video_providers.dart';
 
@@ -14,88 +13,44 @@ class VideoPlayer extends ConsumerStatefulWidget {
 }
 
 class _VideoPlayerState extends ConsumerState<ConsumerStatefulWidget> {
- BetterPlayerController? _testController;
-
- 
-
-  
+  BetterPlayerController testController =
+      BetterPlayerController(BetterPlayerConfiguration());
 
   @override
   void initState() {
     super.initState();
-
-      BetterPlayerController _testController =BetterPlayerController(
+    testController = BetterPlayerController(
       const BetterPlayerConfiguration(
         aspectRatio: 16 / 9,
         fit: BoxFit.contain,
       ),
-      betterPlayerDataSource:
-          BetterPlayerDataSource(BetterPlayerDataSourceType.network, "http://live.roomba.tv/live/achhar.developer@gmail.com/3hSdCcZFs8/1383042.ts",
-              bufferingConfiguration: const BetterPlayerBufferingConfiguration(
-                minBufferMs: 5000,
-                maxBufferMs: 15000,
-                bufferForPlaybackMs: 3000,
-                bufferForPlaybackAfterRebufferMs: 5000,
-              ),
-              liveStream: true),
+      betterPlayerDataSource: BetterPlayerDataSource(
+          BetterPlayerDataSourceType.network,
+          "http://live.roomba.tv/live/achhar.developer@gmail.com/3hSdCcZFs8/1383042.ts",
+          bufferingConfiguration: const BetterPlayerBufferingConfiguration(
+            minBufferMs: 5000,
+            maxBufferMs: 15000,
+            bufferForPlaybackMs: 3000,
+            bufferForPlaybackAfterRebufferMs: 5000,
+          ),
+          liveStream: true),
     );
-    
-    
-    
-    // stController.initialize();
-    _testController.setupDataSource(_testController.betterPlayerDataSource!)
-.then((response) {
-  // Source loaded successfully
-  print("LOAEDD");
-})
-.catchError((error) async {
-  // Source did not load, url might be invalid
- print("ERRROR");
- print(error);
-});
-    
-    
-     
-  
-
-         
-
-    
+    setState(() {});
   }
 
   @override
   void dispose() {
     super.dispose();
-     
   }
 
   @override
   Widget build(BuildContext context) {
     final betterPlayController = ref.watch(betterPlayerController);
 
-    if(  _testController != null &&   _testController!.hasCurrentDataSourceStarted){
-return AspectRatio(
+    return AspectRatio(
         aspectRatio: 16 / 9,
         child: BetterPlayer(
-            controller: BetterPlayerController(
-      const BetterPlayerConfiguration(
-        aspectRatio: 16 / 9,
-        fit: BoxFit.contain,
-      ),
-      betterPlayerDataSource:
-          BetterPlayerDataSource(BetterPlayerDataSourceType.network, "https://rawgit.com/uit2712/Mp3Container/master/tom_and_jerry_31.mp4",
-              bufferingConfiguration: const BetterPlayerBufferingConfiguration(
-                minBufferMs: 5000,
-                maxBufferMs: 15000,
-                bufferForPlaybackMs: 3000,
-                bufferForPlaybackAfterRebufferMs: 5000,
-              ),
-              ),
-    )  // betterPlayController,
-            ));
-    }else{
-      return Text("earching...");
-    }
-    
+          controller: betterPlayController,
+        ));
   }
 }
